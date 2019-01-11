5d2d5ff2f8adf8b442b6b0ac016f19ff60ba3033
Tyler Phillips <phillity@umail.iu.edu>
2019-01-10 20:35:39 -0500

    Async database updates, UI updates

--- DynaSwapApp/views.py
+++ DynaSwapApp/views.py
@@ -1,3 +1,4 @@
+import threading
 import base64
 import os
 import sys
@@ -28,7 +29,23 @@ class RegisterPageView(TemplateView):
 
 class AuthenticatePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'authenticate.html')
+        userName = request.GET.get('userName')
+        roleId = request.GET.get('roleId')
+        return render(request, 'authenticate.html', { 'userName' : userName, 'roleId' : roleId })
+
+class AcceptedPageView(TemplateView):
+    def get(self, request, **kwargs):
+        userName = request.GET.get('userName')
+        confidence = request.GET.get('confidence')
+        roleId = request.GET.get('roleId')
+        return render(request, 'accepted.html', { 'userName' : userName, 'confidence' : confidence, 'roleId' :  roleId})
+
+class RejectedPageView(TemplateView):
+    def get(self, request, **kwargs):
+        userName = request.GET.get('userName')
+        confidence = request.GET.get('confidence')
+        roleId = request.GET.get('roleId')
+        return render(request, 'accepted.html', { 'userName' : userName, 'confidence' : confidence, 'roleId' :  roleId})
 
 class GetRolesView(TemplateView):
     def get(self, request, **kwargs):
@@ -38,6 +55,22 @@ class GetRolesView(TemplateView):
         return JsonResponse({"status": "true", "roles" : roles})
 
 class RegisterView(TemplateView):
+    __reg = register()
+
+    def update_database(self,user_name,role_instance,bcs):
+        # Train classifier using biocapsules
+        classifier = self.__reg.register_classifier(bcs)
+
+        # Convert biocapsules and classifier to binary to save in database
+        classifier_binary = pickle.dumps(classifier)
+        bcs_binary = pickle.dumps(bcs)
+
+        # Save new user into database
+        new_user = Users(user_name=user_name,role=role_instance,bio_capsule=bcs_binary,classifier=classifier_binary)
+        new_user.save()
+        print("register - database updated")
+        return
+
     def post(self, request, **kwargs):
         ### Get POST data form
         try:
@@ -60,7 +93,6 @@ class RegisterView(TemplateView):
             return JsonResponse({"status" : "taken", "error" : "Username " + user_name + " has already been registered."})
         
         ### Process POST data form
-        reg = register()
         try:
             # Get role info corresponding to chosen role
             role_instance = Roles.objects.filter(role_name=role)[0]
@@ -71,25 +103,43 @@ class RegisterView(TemplateView):
             for image in images:
                 stream = image.file
                 image = cv2.imdecode(np.fromstring(stream.getvalue(), dtype=np.uint8), 1)
-                bc = reg.register_image(image,role_id)
+                bc = self.__reg.register_image(image,role_id)
                 bcs = np.vstack([bcs,bc])
-
-            # Train classifier using biocapsules
-            classifier = reg.register_classifier(bcs)
-
-            # Convert biocapsules and classifier to binary to save in database
-            classifier_binary = pickle.dumps(classifier)
-            bcs_binary = pickle.dumps(bcs)
-
-            # Save new user into database
-            new_user = Users(user_name=user_name,role=role_instance,bio_capsule=bcs_binary,classifier=classifier_binary)
-            new_user.save()
         except Exception as e:
             return JsonResponse({"status" : "image", "error" : "Invalid input image. " + str(e)})
+
+        ### Update database
+        try:
+            t = threading.Thread(target=self.update_database,args=(user_name,role_instance,bcs))
+            t.setDaemon(True)
+            t.start()
+        except Exception as e:
+            return JsonResponse({"status" : "error"})
 
         return JsonResponse({"status" : "true"})
         
 class AuthenticateView(TemplateView):
+    __reg = register()
+    __auth = authenticate()
+
+    def update_database(self,user_found,bc,bcs):
+        # Update user bcs
+        bc = np.reshape(np.append(bc,1.),(1,513))
+        bcs = np.vstack([bcs,bc])
+        bcs_binary = pickle.dumps(bcs)
+        user_found.bio_capsule = bcs_binary
+
+        # Update user classifier
+        classifier = self.__reg.register_classifier(bcs)
+        classifier_binary = pickle.dumps(classifier)
+        user_found.classifier = classifier_binary
+
+        # Save updated user into database
+        user_found.last_authenticated = timezone.now()
+        user_found.save()
+        print("authenticate - database updated")
+        return
+
     def post(self, request, **kwargs):
         ### Get POST data form
         try:
@@ -110,8 +160,6 @@ class AuthenticateView(TemplateView):
         user_found = user_found[0]
 
         ### Process POST data form
-        auth = authenticate()
-        reg = register()
         try:
             # Get role info corresponding to chosen role
             role_instance = Roles.objects.filter(role_name=role)[0]
@@ -120,36 +168,35 @@ class AuthenticateView(TemplateView):
             # Convert submited images to biocapsules
             stream = image.file
             image = cv2.imdecode(np.fromstring(stream.getvalue(), dtype=np.uint8), 1)
-            bc = auth.authenticate_image(image,role_id)
+            bc = self.__auth.authenticate_image(image,role_id)
+        except Exception as e:
+            return JsonResponse({"status" : "image", "error" : "Invalid input image. " + str(e)})
 
+        ### Perform classification, Update database
+        try:
             # Get bcs and classifier corresponding to username
             bcs = pickle.loads(user_found.bio_capsule)
             classifier = pickle.loads(user_found.classifier)
 
             # Perform authentication
-            classification, prob = auth.authenticate_classifier(bc,classifier)
+            classification, prob = self.__auth.authenticate_classifier(bc,classifier)
+            prob_string = str(np.around((prob * 100.),3)) + "%"
 
             # Classification outcome
             if classification == False:
-                return JsonResponse({"status" : "false", "error" : "Rejected by " + user_name + " classifier with probility: " + str(prob)})
+                return JsonResponse({"status" : "false", "userName" : user_name, "confidence" : prob_string})
 
-            # Update user 
+            # Update user with authentication time, new bc, new classifier
             if prob > 0.70:
-                # Update user bcs
-                bc = np.reshape(np.append(bc,1.),(1,513))
-                bcs = np.vstack([bcs,bc])
-                bcs_binary = pickle.dumps(bcs)
-                user_found.bio_capsule = bcs_binary
+                t = threading.Thread(target=self.update_database,args=(user_found,bc,bcs))
+                t.setDaemon(True)
+                t.start()
 
-                # Update user classifier
-                classifier = reg.register_classifier(bcs)
-                classifier_binary = pickle.dumps(classifier)
-                user_found.classifier = classifier_binary
-
-            # Save updated user into database
-            user_found.last_authenticated = timezone.now()
-            user_found.save()
+            # Update user with authentication time
+            else:
+                user_found.last_authenticated = timezone.now()
+                user_found.save()
         except Exception as e:
-            return JsonResponse({"status" : "image", "error" : "Invalid input image. " + str(e)})
+            return JsonResponse({"status" : "error"})
 
-        return JsonResponse({"status" : "true", "error" : "Accepted by " + user_name + " classifier with probility: " + str(prob)})
+        return JsonResponse({"status" : "true", "userName" : user_name, "confidence" : prob_string})
