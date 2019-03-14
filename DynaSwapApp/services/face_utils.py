9ad3f5be2027a49490bd480ef5f1d6798e6519fd
Tyler Phillips <phillity@umail.iu.edu>
2019-03-13 21:53:14 -0400

    update face_utils and app data

--- DynaSwapApp/services/face_utils.py
+++ DynaSwapApp/services/face_utils.py
@@ -3,12 +3,13 @@ import numpy as np
 from DynaSwapApp.services.face_models.MTCNN import MtcnnService
 from DynaSwapApp.services.face_models.FNET import FnetService
 
+
 class face_utils:
     ########## PREPROCESSING #########################
-    def align(self,image):
+    def align(self, image):
         # Convert image to BGR if grayscale
         if len(image.shape) < 3:
-            image = cv2.cvtColor(image,cv2.COLOR_GRAY2BGR)
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
         # Get RGB version of BGR OpenCV image
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -23,19 +24,19 @@ class face_utils:
 
         # No faces are detected
         if nrof_faces == 0:
-            raise ValueError("No faces detected in user image! (align)")        
+            raise ValueError("No faces detected in user image! (align)")
             print("No faces detected in user image! (align)")
-    
+
         # Multiple faces are detected
         if nrof_faces > 1:
-            raise ValueError("Multiple faces detected in user image! (align)") 
+            raise ValueError("Multiple faces detected in user image! (align)")
             print("Multiple faces detected in user image! (align)")
-    
+
         # One face is detected
         # Get left and right eye points
-        eye_left = (face_pts[1],face_pts[6])
-        eye_right = (face_pts[0],face_pts[5])
-    
+        eye_left = (face_pts[1], face_pts[6])
+        eye_right = (face_pts[0], face_pts[5])
+
         eye_center = np.zeros((3,))
         eye_center[0] = (eye_left[0] + eye_right[0]) / 2
         eye_center[1] = (eye_left[1] + eye_right[1]) / 2
@@ -67,15 +68,15 @@ class face_utils:
         # Get eye center point after rotation
         eye_center = M @ eye_center
 
-        return self.__crop(image,aligned=True,rot_center=eye_center)
+        return self.__crop(image, aligned=True, rot_center=eye_center)
 
-    def __crop(self,image,out_size=160,margin=44,aligned=False,rot_center=None):
+    def __crop(self, image, out_size=160, margin=44, aligned=False, rot_center=None):
         # Convert image to BGR if grayscale
         if len(image.shape) < 3:
-            image = cv2.cvtColor(image,cv2.COLOR_GRAY2BGR)
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
         # Get RGB version of BGR OpenCV image
-        image_rgb = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # Get face bounding box and landmarks using MTCNN method
         # Face bounding box points [top_left,bottom_right]
@@ -96,83 +97,83 @@ class face_utils:
 
         # Format face boudning box
         bb = np.around(bb[0]).astype(int)
-        bb[0] = np.maximum(bb[0]-margin/2,0)
-        bb[1] = np.maximum(bb[1]-margin/2,0)
-        bb[2] = np.maximum(bb[2]+margin/2,0)
-        bb[3] = np.maximum(bb[3]+margin/2,0)
+        bb[0] = np.maximum(bb[0] - margin / 2, 0)
+        bb[1] = np.maximum(bb[1] - margin / 2, 0)
+        bb[2] = np.maximum(bb[2] + margin / 2, 0)
+        bb[3] = np.maximum(bb[3] + margin / 2, 0)
 
         face_bb = []
-        for i in range(0,3,2):
-            face_bb.append((bb[i],bb[i+1]))
+        for i in range(0, 3, 2):
+            face_bb.append((bb[i], bb[i + 1]))
 
         # Get face detection
-        face = image[face_bb[0][1]:face_bb[1][1],face_bb[0][0]:face_bb[1][0]]
-        
+        face = image[face_bb[0][1]:face_bb[1][1], face_bb[0][0]:face_bb[1][0]]
+
         # Resize face image
-        face = cv2.resize(face,(out_size,out_size))
+        face = cv2.resize(face, (out_size, out_size))
 
         # Return detected face
         return face
     ##################################################
 
     ########## FEATURE EXTRACTION ####################
-    def extract(self,image):
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    def extract(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         feature = FnetService().feature(image)
         return feature
     ##################################################
 
     ########## BC GENERATION #########################
-    def bc_fusion(self,user_id,user_feature,rs_feature):
+    def bc_fusion(self, user_id, user_feature, rs_feature):
         user_signature = self.__signature_extraction(user_feature)
         user_key = self.__key_generation(user_signature)
 
         rs_signature = self.__signature_extraction(rs_feature)
         rs_key = self.__key_generation(rs_signature)
 
-        bc = np.multiply(user_feature,rs_key) + np.multiply(rs_feature,user_key)
-        bc = np.reshape(np.append(bc,user_id),(1,513))
+        bc = np.multiply(user_feature, rs_key) + np.multiply(rs_feature, user_key)
+        bc = np.reshape(np.append(bc, user_id), (1, 513))
         return bc
 
-    def __signature_extraction(self,feature):
-        feature_grid = np.reshape(feature,(32,16))
+    def __signature_extraction(self, feature):
+        feature_grid = np.reshape(feature, (32, 16))
         LTP = np.zeros(feature_grid.shape)
         inc = 3
-        B_size = np.floor(np.array([3,3]) / 2).astype(int)
-        T_size = np.floor(np.array([5,5]) / 2).astype(int)
-        for r in range(0,feature_grid.shape[0],inc):
-            for c in range(0,feature_grid.shape[1],inc):
+        B_size = np.floor(np.array([3, 3]) / 2).astype(int)
+        T_size = np.floor(np.array([5, 5]) / 2).astype(int)
+        for r in range(0, feature_grid.shape[0], inc):
+            for c in range(0, feature_grid.shape[1], inc):
                 # Get T window
-                T_r_s = max([0,r-T_size[0]])
-                T_r_e = min([feature_grid.shape[0],r+T_size[0]])
-                T_c_s = max([0,c-T_size[1]])
-                T_c_e = min([feature_grid.shape[1],c+T_size[0]])
-                T = feature_grid[T_r_s:T_r_e+1,T_c_s:T_c_e+1]
+                T_r_s = max([0, r - T_size[0]])
+                T_r_e = min([feature_grid.shape[0], r + T_size[0]])
+                T_c_s = max([0, c - T_size[1]])
+                T_c_e = min([feature_grid.shape[1], c + T_size[0]])
+                T = feature_grid[T_r_s:T_r_e + 1, T_c_s:T_c_e + 1]
 
                 # Get T window mean
                 T_mean = np.mean(T)
 
                 # Get B window
-                B_r_s = max([0,r-B_size[0]])
-                B_r_e = min([feature_grid.shape[0],r+B_size[0]])
-                B_c_s = max([0,c-B_size[1]])
-                B_c_e = min([feature_grid.shape[1],c+B_size[0]])
-                B = feature_grid[B_r_s:B_r_e+1,B_c_s:B_c_e+1]
-                LTP[B_r_s:B_r_e+1,B_c_s:B_c_e+1] = np.abs(B - T_mean)
+                B_r_s = max([0, r - B_size[0]])
+                B_r_e = min([feature_grid.shape[0], r + B_size[0]])
+                B_c_s = max([0, c - B_size[1]])
+                B_c_e = min([feature_grid.shape[1], c + B_size[0]])
+                B = feature_grid[B_r_s:B_r_e + 1, B_c_s:B_c_e + 1]
+                LTP[B_r_s:B_r_e + 1, B_c_s:B_c_e + 1] = np.abs(B - T_mean)
 
-        LTP = np.around(np.average(LTP,axis=1) * 100).astype(int)
+        LTP = np.around(np.average(LTP, axis=1) * 100).astype(int)
         return LTP
 
-    def __key_generation(self,signature):
+    def __key_generation(self, signature):
         key = np.zeros((512,))
         idx = 0
-        for i,s in enumerate(signature):
+        for i, s in enumerate(signature):
             np.random.seed(s)
             for j in range(16):
                 key[idx] = np.random.rand()
                 idx = idx + 1
 
         key = np.around(key)
-        key = (key * 2) -1
+        key = (key * 2) - 1
         return key
     ##################################################
